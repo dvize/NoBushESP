6619d83e9f9e1300f6d21fc1aa0b267bcdbc4e44
dvize <daniel.m.han@gmail.com>
2022-12-17 08:00:37 -0800

    Add project files.

---
+++ AIPatcher.cs
@@ -0,0 +1,57 @@
+﻿using Aki.Reflection.Patching;
+using EFT;
+using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace NoBushESP
+{
+    public class AIESPPatcherAimPlayer : ModulePatch
+    {
+
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(BotGroupClass).GetMethod("CalcGoalForBot");
+        }
+
+        [PatchPostfix]
+        public static void PatchPostfix(BotOwner bot)
+        {
+            //Need when enemy is alerted to player 
+
+            object goalEnemy = bot.Memory.GetType().GetProperty("GoalEnemy").GetValue(bot.Memory);
+
+            if (goalEnemy != null)
+            {
+                IAIDetails person = (IAIDetails)goalEnemy.GetType().GetProperty("Person").GetValue(goalEnemy);
+                
+                if (person.GetPlayer.IsYourPlayer)
+                {
+                    RaycastHit hitInfo;
+                    float radius = 1.0f;  //need to tweak this for width
+                    float maxdistance = 137f; // what is bots vision range.  137f
+                    LayerMask layermask = 1 << 0 | 1 << 11 | 1 << 26;
+
+
+                    //bool isVisible = (bool)goalEnemy.GetType().GetProperty("IsVisible").GetValue(goalEnemy);
+
+                    if (Physics.SphereCast(bot.gameObject.transform.position, radius, person.GetPlayer.gameObject.transform.forward, out hitInfo, maxdistance, layermask))
+                    {
+                        //Logger.LogInfo("Object Name: " + hitInfo.collider.gameObject.name);
+                        //Logger.LogInfo("Object Layer: " + hitInfo.collider.gameObject.layer);
+
+                        if ((hitInfo.collider.transform.parent?.gameObject?.name?.Contains("filbert") == true) || (hitInfo.collider.transform.parent?.gameObject?.name?.Contains("fibert") == true))
+                        {
+                            //Logger.LogInfo("filbert or Fibert in the way");
+                            bot.Memory.GetType().GetProperty("GoalEnemy").SetValue(bot.Memory, null);
+                        }
+
+                    }
+
+                }
+            }
+
+        }
+    }
+}
---
+++ AIPatcherPlugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+using EFT;
+using Comfort.Common;
+using System.Diagnostics.Tracing;
+
+namespace NoBushESP
+{
+
+    [BepInPlugin("com.fenix.NoAI_ESP", "Fenix.NoAI_ESP", "1.0.0")]
+    class AIPatcherPlugin : BaseUnityPlugin
+    {
+
+        private void Awake()
+        {
+
+            new AIESPPatcherAimPlayer().Enable();
+            
+        }
+        
+    }
+}
