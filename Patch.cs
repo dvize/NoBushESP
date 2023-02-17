8eab3e430d0bf458162ebfcb01ba01c3caa7a8dd
dvize <daniel.m.han@gmail.com>
2023-02-17 10:51:17 -0800

    Added try catch for NRE errors. Also should include more layers in the layer mask.

--- Patch.cs
+++ Patch.cs
@@ -22,81 +22,100 @@ namespace NoBushESP
 
         public static WildSpawnType[] scavList = { WildSpawnType.assault, WildSpawnType.marksman, WildSpawnType.cursedAssault, WildSpawnType.assaultGroup };
 
-        public static List<string> exclusionList = new List<string> { "filbert", "fibert", "tree", "pine", "plant" };
+        public static List<string> exclusionList = new List<string> { "filbert", "fibert", "tree", "trees", "pine", "plant"};
         protected override MethodBase GetTargetMethod()
         {
-            return typeof(BotGroupClass).GetMethod("CalcGoalForBot");
+            try
+            {
+                return typeof(BotGroupClass).GetMethod("CalcGoalForBot");
+            }
+            catch
+            {
+                Logger.LogInfo("NoBushESP: Failed to get target method.. target dead or unspawned.");
+            }
+
+            return null;
         }
 
         [PatchPostfix]
         public static void PatchPostfix(BotOwner bot)
         {
-            //Need when enemy is alerted to player 
-
-            object goalEnemy = bot.Memory.GetType().GetProperty("GoalEnemy").GetValue(bot.Memory);
-
-            if (goalEnemy != null)
+            try
             {
-                IAIDetails person = (IAIDetails)goalEnemy.GetType().GetProperty("Person").GetValue(goalEnemy);
+                //Need when enemy is alerted to player 
 
-                if (person.GetPlayer.IsYourPlayer)
+                object goalEnemy = bot.Memory.GetType().GetProperty("GoalEnemy").GetValue(bot.Memory);
+
+                if (goalEnemy != null)
                 {
-                    //skip if config says disabled
-                    if (scavList.Contains(bot.Profile.Info.Settings.Role) && NoBushESPPlugin.ScavsStillSee.Value)
+                    IAIDetails person = (IAIDetails)goalEnemy.GetType().GetProperty("Person").GetValue(goalEnemy);
+
+                    if (person.GetPlayer.IsYourPlayer)
                     {
-                        return;
-                    }
-
-                    if (bossesList.Contains(bot.Profile.Info.Settings.Role) && NoBushESPPlugin.BossesStillSee.Value)
-                    {
-                        return;
-                    }
-
-                    if (followersList.Contains(bot.Profile.Info.Settings.Role) && NoBushESPPlugin.BossFollowersStillSee.Value)
-                    {
-                        return;
-                    }
-
-                    if (pmcList.Contains(bot.Profile.Info.Settings.Role) && NoBushESPPlugin.PMCsStillSee.Value)
-                    {
-                        return;
-                    }
-
-                    RaycastHit hitInfo;
-                    LayerMask layermask = 1 << 0 | 1 << 11 | 1 << 26;
-
-                    float maxdistance = Vector3.Distance(bot.Position, person.GetPlayer.Position);
-
-                    if (Physics.SphereCast(bot.Position, NoBushESPPlugin.TestRayRadius.Value, person.GetPlayer.Position, out hitInfo, maxdistance, layermask))
-                    {
-                        //Logger.LogInfo("Object Name: " + hitInfo.collider.gameObject.name);
-                        //Logger.LogInfo("Object Layer: " + hitInfo.collider.gameObject.layer);
-
-                        foreach (string exclusion in exclusionList)
+                        //skip if config says disabled
+                        if (scavList.Contains(bot.Profile.Info.Settings.Role) && NoBushESPPlugin.ScavsStillSee.Value)
                         {
-                            if ((bool)(hitInfo.collider.transform.parent?.gameObject?.name.ToLower().Contains(exclusion)))
+                            return;
+                        }
+
+                        if (bossesList.Contains(bot.Profile.Info.Settings.Role) && NoBushESPPlugin.BossesStillSee.Value)
+                        {
+                            return;
+                        }
+
+                        if (followersList.Contains(bot.Profile.Info.Settings.Role) && NoBushESPPlugin.BossFollowersStillSee.Value)
+                        {
+                            return;
+                        }
+
+                        if (pmcList.Contains(bot.Profile.Info.Settings.Role) && NoBushESPPlugin.PMCsStillSee.Value)
+                        {
+                            return;
+                        }
+
+                        RaycastHit hitInfo;
+                        LayerMask layermask = 1 << 0 | 1 << 11 | 1 << 12 | 1 << 19 | 1 << 26 | 1 << 31 | 1 << 32;
+
+                        float maxdistance = Vector3.Distance(bot.Position, person.GetPlayer.Position);
+
+                        if (Physics.SphereCast(bot.Position, NoBushESPPlugin.TestRayRadius.Value, person.GetPlayer.Position, out hitInfo, maxdistance, layermask))
+                        {
+                            //Logger.LogInfo("Object Name: " + hitInfo.collider.gameObject.name);
+                            //Logger.LogInfo("Object Layer: " + hitInfo.collider.gameObject.layer);
+
+                            foreach (string exclusion in exclusionList)
                             {
-                                //Logger.LogInfo("filbert or Fibert in the way");
-
-                                // Logger.LogInfo("Setting IsVisible to false for: " + bot.Profile.Info.Settings.Role);
-
-                                if (NoBushESPPlugin.BlockingTypeGoalEnemy.Value == true)
+                                if ((bool)(hitInfo.collider.transform.parent?.gameObject?.name.ToLower().Contains(exclusion)))
                                 {
-                                    bot.Memory.GetType().GetProperty("GoalEnemy").SetValue(bot.Memory, null);
-                                }
-                                else
-                                {
-                                    goalEnemy.GetType().GetProperty("IsVisible").SetValue(goalEnemy, false);
-                                }
+                                    //Logger.LogInfo("Some object is in the way of the bot.");
 
+                                    //Logger.LogInfo("Setting IsVisible to false for: " + bot.Profile.Info.Settings.Role);
+
+                                    if (NoBushESPPlugin.BlockingTypeGoalEnemy.Value == true)
+                                    {
+                                        bot.Memory.GetType().GetProperty("GoalEnemy").SetValue(bot.Memory, null);
+                                        Logger.LogInfo("NoBushESP: Blocking GoalEnemy for: " + bot.Profile.Info.Settings.Role);
+                                    }
+                                    else
+                                    {
+                                        goalEnemy.GetType().GetProperty("IsVisible").SetValue(goalEnemy, false);
+                                        Logger.LogInfo("NoBushESP: Setting IsVisible to false for: " + bot.Profile.Info.Settings.Role);
+                                    }
+
+
+                                }
 
                             }
-
                         }
+
+
                     }
-
-
                 }
+
+            }
+            catch
+            {
+                Logger.LogInfo("NoBushESP: Cannot Assign Brain Because Enemy is Dead or Unspawned");
             }
 
         }
